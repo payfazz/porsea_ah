dc708725415d0b78369834926d16c0d4eae6867f
azharprabudi <azharprabui@gmail.com>
2019-03-20 20:00:10 +0700

    example config webpack

--- config/webpack/default.webpack.config.js
+++ config/webpack/default.webpack.config.js
@@ -1,8 +1,19 @@
+// Porsea framework
+// Created by payfazz team
+// If you want to contribute visit here : https://github.com/payfazz/porsea
 var path = require("path");
 
+var rootPath = `${__dirname}/../..`;
+var mode = process.env.NODE_ENV || "development";
+
 var config = {
+  mode: mode,
   entry: {
-    entry: path.joi
+    main: path.resolve(rootPath, "container/index.js")
+  },
+  output: {
+    path: path.resolve(rootPath, "build"),
+    filename: "[name].[contenthash].js"
   }
 };
 
