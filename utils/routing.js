0fae12675bd1deb4ab68a25516f145de080b7360
azharprabudi <azharprabui@gmail.com>
2019-04-30 10:35:05 +0700

    add not found page

--- utils/routing.js
+++ utils/routing.js
@@ -1,26 +1,34 @@
 import React from "react";
-import { Route, Link } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
+import NotFoundPage from "../../../src/pages/not-found";
 import compose from "./helper";
 
 const filterIndexJSIsExist = pages =>
   pages.filter(({ isIndexJSExists }) => isIndexJSExists);
 
-const mapObjectToRoute = pages =>
-  pages.map(({ folderName }) => {
-    const Page = require("../../../src/pages/" + folderName).default;
-    if (Page.navigationOptions) {
-      return (
-        <Route
-          key={Page.navigationOptions().path}
-          component={Page}
-          {...Page.navigationOptions()}
-        />
-      );
-    }
+const mapObjectToRoute = pages => {
+  return (
+    <Switch>
+      {pages.map(({ folderName }) => {
+        const Page = require("../../../src/pages/" + folderName).default;
+        if (Page.navigationOptions) {
+          return (
+            <Route
+              key={Page.navigationOptions().path}
+              component={Page}
+              {...Page.navigationOptions()}
+            />
+          );
+        }
 
-    return <Route key={folderName} component={Page} path={"/" + folderName} />;
-  });
-
+        return (
+          <Route key={folderName} component={Page} path={"/" + folderName} />
+        );
+      })}
+      <Route path={"*"} component={NotFoundPage} />
+    </Switch>
+  );
+};
 const folderInPages = JSON.parse(process.env.FOLDER_IN_PAGES);
 const generateRoute = () =>
   compose(
